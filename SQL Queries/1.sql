1bd01b849128df1c15e5220a5f275db03e42ea92
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2026-02-02 07:57:34 +0300

    final

--- SQL Queries/1.sql
+++ SQL Queries/1.sql
@@ -15,4 +15,10 @@ FROM customers;
 SELECT COUNT ([employee_name]) AS Total_number_of_employee
 FROM [HackerRankSQL].[dbo].[employees]
 
- 
+  -- show all products with price greater than 200 --
+SELECT TOP (1000) [product_id]
+      ,[product_name]
+      ,[price]
+  FROM [HackerRankSQL].[dbo].[products]
+  WHERE [price] > 200
+  ORDER BY  [price] DESC;
